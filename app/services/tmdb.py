09035e04a04db50996f584bf08bb96f9c4ef1f75
Adrian Uchacz <a.uchacz@op.pl>
2026-04-29 12:16:37 +0200

    Add TMDB search movies service

---
+++ app/services/tmdb.py
@@ -0,0 +1,25 @@
+import requests
+from flask import current_app
+
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
+TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
+def search_movies(query: str) -> list:
+    url = f"{TMDB_BASE_URL}/search/movie"
+    headers = {
+        "Authorization": f"Bearer {current_app.config['TMDB_API_KEY']}"
+    }
+    params = {
+        "query": query,
+        "language": "en-US",
+    }
+
+    response = requests.get(url, headers=headers, params=params, timeout=5)
+    response.raise_for_status()
+
+    movies = response.json().get("results", [])
+    for movie in movies:
+        poster = movie.get("poster_path")
+        movie["poster_url"] = f"{TMDB_IMAGE_BASE_URL}{poster}" if poster else None
+
+    return movies
