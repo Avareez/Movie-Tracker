2eaa497e2a0ed94734c8b799d1ac9a1cbc1fa3b3
Adrian Uchacz <a.uchacz@op.pl>
2026-05-06 23:01:23 +0200

    Add JWT manager to app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
     db.init_app(app)
     login_manager.init_app(app)
+    jwt.init_app(app)
     login_manager.login_view = "auth.login"
 
     with app.app_context():
--- app/config.py
+++ app/config.py
@@ -8,3 +8,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///movie_tracker.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
